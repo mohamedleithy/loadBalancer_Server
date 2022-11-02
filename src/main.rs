8b815eacd4eff81e765d3d7ee7022a0b9e9824fd
abdallahmohamed <abdallah8399@aucegypt.edu>
2022-11-02 14:52:23 +0200

    servers send and receive in different threads

--- src/main.rs
+++ src/main.rs
@@ -10,34 +10,53 @@ fn main() -> std::io::Result<()>{
     {
 
 
-        // thread to comunicate with the other servers
+        // thread to send messages to other servers
         let handler = thread::spawn(move || {
-        let socket = UdpSocket::bind("172.20.10.5:5960").unwrap();
+        let socket = UdpSocket::bind("172.20.10.13:5966").unwrap();
 
             while true {
                 println!("Sending message to other servers");
                 let mut rng = rand::thread_rng();
-                let n = rng.gen_range(0, 5);
+                let n = rng.gen_range(0, 6);
                 println!("Random number: {n}");
                 
                 if n == 5{
-                    socket.send_to(b"Server 1 down", "172.20.10.2:34255").unwrap();
-                    socket.send_to(b"Server 1 down", "172.20.10.3:34255").unwrap();
+                    // socket.send_to(b"Server 1 down", "172.20.10.13:34255").unwrap();
+                    socket.send_to(b"Server 1 down", "172.20.10.13:5960").unwrap();
                 }
 
-                let mut buf = [0; 10];
-                let (amt, src) = socket.recv_from(&mut buf).unwrap();
-               thread::sleep(Duration::from_millis(100));
+               thread::sleep(Duration::from_millis(1000));
     
-                // Redeclare `buf` as slice of the received data and send reverse data back to origin.
-                let buf = &mut buf[..amt];
-                socket.send_to(buf, &src).unwrap();
             }
             
             });
 
 
-        let socket = UdpSocket::bind("172.20.10.5:5959").unwrap();
+            // thread to receive messages from other servers
+            let handler1 = thread::spawn(move || {
+                let socket = UdpSocket::bind("172.20.10.13:5960").unwrap();
+        
+                while true {
+                        println!("Recieving messages from other servers");
+
+                        let mut buf = [0; 30];
+                        let (amt, src) = socket.recv_from(&mut buf).unwrap();
+                        println!("Message Recieved!");
+                        // println!("Message: {:?}", &buf[..amt]);
+                        println!("From: {:?}", src);
+                        //print the received data as a string
+                        println!("Message: {}", String::from_utf8_lossy(&buf));
+
+
+
+                        thread::sleep(Duration::from_millis(1000));
+                 
+                    }
+                        
+                });
+
+
+        let socket = UdpSocket::bind("172.20.10.13:5959").unwrap();
 
         // main thread to comunicate with the clients
         while true {
@@ -53,6 +72,7 @@ fn main() -> std::io::Result<()>{
 
 
         handler.join().unwrap();
+        handler1.join().unwrap();
     }
 
     Ok(())
