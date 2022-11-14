7b003b0db22d204fcb15020dd34b2f4fbcdadebe
mohamedleithy <leithy@aucegypt.edu>
2022-11-14 11:35:49 +0200

    sending system stats to other servers

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 // importing the udp standard library
 
 extern crate systemstat;
+extern crate local_ip;
 
 use std::net::UdpSocket;
 use std::fs::File;
@@ -10,98 +11,109 @@ use std::time::Duration;
 use rand::Rng;
 use rand::seq::index;
 use std::sync::{Arc, Mutex};
+use systemstat::{System, Platform, saturating_sub_bytes};
 
 use std::time;
 
-
+// To store server status
 struct server{
 
     ip: String,
     state: bool,
+    temperature: u8,
 }
 
 fn main() -> std::io::Result<()>{
     {
 
         // The client should be aware of servers that are up, from the leader 
-      let mut temp: [server; 3] = [server { ip: "172.20.10.6:2024".to_string(), state: true }, server { ip: "172.20.10.6:2024".to_string(), state: true }, server { ip: "192.168.1.3:2024".to_string(), state: true },];
-      let serverIps = Arc::new(Mutex::new(temp));
-
-        let serverIps1 = Arc::clone(&serverIps);
-
-
-        // LEITHY PC -> INDEX 0 
-        // MARK PC -> INDEX  1 
-        // THESIS PC -> INDEX 2
-
-        
-        // send to servers current memory usage as a parameter to elect the server to go down
+      let temp: [server; 3] = [server { ip: "172.20.10.6:2024".to_string(), state: true, temperature: 100}, server { ip: "172.20.10.6:2024".to_string(), state: true, temperature: 100 }, server { ip: "172.20.10.3:2024".to_string(), state: true, temperature: 100},];
+      let serverInfo = Arc::new(Mutex::new(temp));
+      let ip = local_ip::get().unwrap();
+        // send to servers current temperature as a parameter to elect the server to go down
+        let serverInfo1 = Arc::clone(&serverInfo);
         let handler = thread::spawn( move || {
-        let socket = UdpSocket::bind("172.20.10.3:2025").unwrap();
+        let sys = System::new();
+        let socket = UdpSocket::bind(ip.to_string() + ":2025").unwrap();
         let serverOutThreadMsg = "serverOutThread::";
             loop {
-                println!("{} Sending message to other servers", serverOutThreadMsg);
-              
-            
-                    let mut num = serverIps1.lock().unwrap();
-                    num[0].state = false;
-                        // I am server 0
-                        socket.send_to(b"0", &num[1].ip).unwrap();
-                        // socket.send_to(b"0", &num[2].ip).unwrap();
 
-                        std::mem::drop(num); 
+                let cpu_temp = sys.cpu_temp().unwrap();
+                
+                println!("{} Sending message to other servers. Current temp: {}", serverOutThreadMsg, cpu_temp);
+    
+                let mut serverInfo11 = serverInfo1.lock().unwrap();
+                let msg = format!("Temprature:{}", cpu_temp);
+                let mut buffer = msg.as_bytes();
 
-            // every one minute send your current memory usage to the neighboring servers
+
+                for server in serverInfo11.iter(){
+                    if server.ip != ip.to_string() + ":2025" {
+                        socket.send_to(&buffer , &server.ip).unwrap();
+                    }
+                }
+                println!("{}", serverInfo11[2].temperature.to_string());
+                std::mem::drop(serverInfo11); 
+
+            // every one minute send your current temperature to the neighboring servers
             let one_minute = time::Duration::from_millis(60000);
             thread::sleep(one_minute);
+            
     
             }
             
             });
 
-            let serverIps2 = Arc::clone(&serverIps);
+
+            let serverInfo2 = Arc::clone(&serverInfo);
             // thread to receive messages from other servers, to update table
             // dedicated thread to avoid being blocked by recv_from
 
             
             let handler1 = thread::spawn(move || {
-                let socket = UdpSocket::bind("172.20.10.3:2024").unwrap();
+                let socket = UdpSocket::bind(ip.to_string()+":2024").unwrap();
                 let serverInThreadMsg = "serverInThread::";
                 loop {
                     println!("{} Receiving message from other servers", serverInThreadMsg);
                         
-                        let mut buf = [0; 1]; // buffer for recieving 
+                        let mut buf = [0; 100]; // buffer for recieving 
 
 
                         // blocked till Recieving a message from any of the other servers 
 
                         let (amt, src) = socket.recv_from(&mut buf).unwrap();
-                        let mut num = serverIps2.lock().unwrap();
-                        if &buf == b"0"{
-                            println!("Server 0 Dropped", serverInThreadMsg);
-                            num[0].state = false;
-                        }  else if &buf == b"1"{
-                            println!("Server 1 Dropped", serverInThreadMsg);
-                            num[1].state = false;
-                        } else if &buf == b"2"{
-                            println!("Server 2 Dropped", serverInThreadMsg);
-                            num[2].state = false;
-                        }
-                        std::mem::drop(num);
+                        let mut serverInfo22 = serverInfo2.lock().unwrap();
+            
+                        
 
                         println!("Message Recieved!");
 
                         println!("From: {:?}", src);
                         //print the received data as a string 
+                        let t = &buf[11..15];
+                        println!("Message: {}", String::from_utf8_lossy(&t));
+                       
 
-                        println!("Message: {}", String::from_utf8_lossy(&buf));
-                 
+                        // update the corresponding server in the serverInfo list
+
+                        for server in serverInfo22.iter_mut(){
+                            if src.to_string() == (server.ip.to_string()+ ":2025")  {
+                                println!("hey"); 
+                                server.temperature = String::from_utf8((&t).to_vec()).unwrap().parse::<u8>().unwrap();
+                            }
+
+                        }
+
+                        std::mem::drop(serverInfo22);
+
+                        
+                       
                     }
                         
                 });
 
 
-        let socket = UdpSocket::bind("172.20.10.3:2023").unwrap();
+        let socket = UdpSocket::bind(ip.to_string()+":2023").unwrap();
         let agentsThreadMsg = "agentsThread::";
         // main thread to comunicate with the agents to perform main server functionality (reverse word)
         loop {
